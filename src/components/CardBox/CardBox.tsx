64d4458c55aa07fabea45d1ae4ec6b23d4b81544
Artis Vansovics <vansovics.artis@gmail.com>
2022-04-28 16:40:36 +0300

    Added reset to Card container

--- src/components/CardBox/CardBox.tsx
+++ src/components/CardBox/CardBox.tsx
@@ -9,37 +9,31 @@ const CardBox = () => {
       idCard: 1,
       title: 'First card',
       key: 'card_01',
-      show: true,
     },
     {
       idCard: 2,
       title: 'Second card',
       key: 'card_02',
-      show: true,
     },
     {
       idCard: 3,
       title: 'Third card',
       key: 'card_03',
-      show: true,
     },
     {
       idCard: 4,
       title: 'Fourth card',
       key: 'card_04',
-      show: true,
     },
     {
       idCard: 5,
       title: 'Fifth card',
       key: 'card_05',
-      show: true,
     },
     {
       idCard: 6,
       title: 'Sixth card',
       key: 'card_06',
-      show: true,
     },
   ];
   const [cards, setCards] = useState([...initialCards]);
@@ -47,13 +41,7 @@ const CardBox = () => {
     {
       title: 'Reset',
       onClick: () => {
-        setCards([...initialCards.map(({
-          idCard, title, key, show,
-        }) => (
-          {
-            idCard, show, key: key.toUpperCase(), title,
-          }
-        ))]);
+        setCards([...initialCards]);
       },
     },
     {
@@ -66,17 +54,15 @@ const CardBox = () => {
       title: 'Make all titles UPPERCASE',
       onClick: () => {
         setCards([...cards.map(({
-          idCard, title, key, show,
+          idCard, title, key,
         }) => (
           {
-            idCard, show, key, title: title.toUpperCase(),
+            idCard, key, title: title.toUpperCase(),
           }
         ))]);
       },
     },
   ];
-  console.log(cards);
-  console.log(initialCards);
 
   return (
     <div className="card-box">
@@ -87,9 +73,9 @@ const CardBox = () => {
       </div>
       <div className="card-box__row">
         {[...cards.map(({
-          idCard, title, show, key,
+          idCard, title, key,
         }) => (
-          <Card key={key} idCard={idCard} title={title} show={show} />
+          <Card key={key} idCard={idCard} title={title} allCards={cards} removeCard={setCards} />
         ))]}
       </div>
     </div>
